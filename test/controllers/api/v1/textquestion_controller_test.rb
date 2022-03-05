683af95802dd5c844fadf9256d43fed601fa57cf
Aashna <aashnanarang33@gmail.com>
2022-03-05 15:10:12 -0500

    Add text question unit tests

--- test/controllers/api/v1/textquestion_controller_test.rb
+++ test/controllers/api/v1/textquestion_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class Api::V1::TextquestionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    survey = surveys(:one)
+    @survey = Survey.new({ isLive: survey.isLive, title: survey.title})
+    @survey2 = surveys(:two)
+    @textquestion = textquestions(:one)
+    @textquestion.survey_id = @survey.id
+    @textquestion2 = textquestions(:two)
+    @textquestion2.survey_id = @survey.id
+  end
+
+  test "should create text question" do
+    assert_difference("TextQuestion.count") do
+      post "/textquestions/create", params: { textquestion: { question: @textquestion.question, survey_id: @survey.id} }
+    end
+    # assert reponse = TextQuestion.last and fields = the ones inputted
+  end
+  
+  test "should update text question" do
+    patch "/textquestions/#{@textquestion.id}", params: { textquestion: { question: "new question"} }
+    # assert reponse = TextQuestion.last and fields = the ones inputted
+  end
+
+  test "should update text question and change survey id" do
+    patch "/textquestions/#{@textquestion.id}", params: { textquestion: { question: "new question", survey_id: @survey2.id } }
+    # assert reponse = TextQuestion.last and fields = the ones inputted
+  end
+
+  test "should destroy textquestion" do
+    assert_difference("Survey.count", -1) do
+      delete "/textquestions/#{@textquestion.id}"
+    end
+  end
+
 end
