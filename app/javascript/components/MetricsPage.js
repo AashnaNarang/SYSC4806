0683549008d7d8334341186796fc52e013f2f65d
Vasugi14 <vasugi.ganesh@hotmail.com>
2022-04-03 16:26:17 -0400

    atleast something that shows up

--- app/javascript/components/MetricsPage.js
+++ app/javascript/components/MetricsPage.js
@@ -12,46 +12,79 @@ const darkTheme = createTheme({ palette: { mode: 'dark' } });
 const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 const MetricsPage = () => {
-    const { surveyId } = useParams();
-    const [baseUrl, setBaseUrl] = useState('');
-    const [title, setTitle] = useState('');
+    console.log("checker 1")
+    // const { surveyId } = useParams();
+    // const [baseUrl, setBaseUrl] = useState('');
+    // const [title, setTitle] = useState('');
+    
+    // const checkRequest = (res) => {
+    //     if (res.status === 200) {
+    //         return res.json();
+    //     } else {
+    //         throw res;
+    //     }
+    // }
 
-    const checkRequest = (res) => {
-        if (res.status === 200) {
-            return res.json();
-        } else {
-            throw res;
-        }
-    }
+    // useEffect(async () => {
+    //     console.log("checker 2")
+    //     setBaseUrl(window.location.origin.replace(/\/#.*/, ""));
+    //     await fetch(`${baseUrl}/api/v1/surveyResponses/${surveyId}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //     })
+    //     .then(checkRequest)
+    //     .then(data => {
+    //         setTitle(data.survey.title);
+    //     })
+    //     .catch(console.log);
+    // }, []);
 
-    useEffect(async () => {
-        setBaseUrl(window.location.origin.replace(/\/#.*/, ""));
-        await fetch(`${baseUrl}/api/v1/surveyResponses/${surveyId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(checkRequest)
-        .then(data => {
-            setTitle(data.survey.title);
-        })
-        .catch(console.log);
-    }, []);
 
     return(
-        <Card>
-            <ThemeProvider theme={darkTheme}>
-                <Typography variant="h4" component="h3">
-                    Hello surveyor, Welcome to the Metrics Page!
-                </Typography> 
-                <Paper variant="outlined" />
-            </ThemeProvider>
-            <br/>
-            <br/>
-            {/* <SurveyList></SurveyList> */}
-        </Card>
+        <div className="surveyResponses">
+            <Card>
+                <ThemeProvider theme={darkTheme}>
+                    <Typography variant="h4" component="h3">
+                        Hello surveyor, Welcome to the Metrics Page!
+                    </Typography> 
+                    <Paper variant="outlined" />
+                </ThemeProvider>
+                <br/>
+                <br/>
+            </Card>
+        </div>
     )
+
+    // return(
+    //     <div className="surveyResponses">
+    //         <Typography variant="h2">{title}</Typography>
+    //         <br/>
+    //         <br/>
+    //         <Paper
+    //             sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 600 }}
+    //         >
+    //             <Box
+    //                 sx={{
+    //                 '& .MuiTextField-root': { m: 1, width: '25ch' },
+    //                 }}
+    //             >
+    //                 <br/>
+    //                 <Button
+    //                         variant="text"                              
+    //                         color="secondary"
+    //                         size="small"
+    //                         onClick={handleSubmitSurvey}
+    //                 >Back</Button>
+    //             </Box>
+                
+    //         </Paper>
+    //         <br/>
+    //         <br/>
+
+    //     </div>
+    // )
 }
 
 export default MetricsPage
