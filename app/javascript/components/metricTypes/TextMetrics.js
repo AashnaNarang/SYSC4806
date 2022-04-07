9b2d8700d14d771d4be2fcf5a909daf5d5f4ff69
JudyHamwi <55767330+JudyHamwi@users.noreply.github.com>
2022-04-07 01:39:53 -0400

    Created a TextMetrics component

---
+++ app/javascript/components/metricTypes/TextMetrics.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { TextField, Typography } from '@mui/material'
+
+export default function TextMetrics(response) {
+  return (
+    <>
+        <Typography variant="h5">Summary of Open Ended Questions:</Typography>
+        <TextField>
+            disabled
+            value=Object.keys(response.question)
+            variant="outlined"
+            label="Question"
+            size="small"
+        </TextField>
+        <TextField> 
+            disabled
+            multiline          
+            label="Answer List"
+            value=Object.keys(response.text_response)
+            variant="outlined"
+            size="small"
+        </TextField>
+    </>
+  )
+}
